0199bea843160b5a62f0d0ea79cc8a9f47ddac2b
Fabio Brito <fabio.britto15@gmail.com>
2020-05-02 18:04:12 -0300

    desing pattern and SOLID

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,39 @@
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import { startOfHour } from 'date-fns';
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+// Dependency Inversion (SOLID)
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider }: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      date,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw new Error('This appointment is already booked');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
