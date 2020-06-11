90e9223e7aa9e93130908683d00542c7dba95d17
Fabio Brito <fabio.britto15@gmail.com>
2020-06-11 13:12:49 -0300

    ddd refactor

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,8 +1,8 @@
 import { getCustomRepository } from 'typeorm';
 
-import AppError from '../errors/AppError';
+import AppError from '@shared/errors/AppError';
 
-import Appointment from '../models/Appointment';
+import Appointment from '../infra/typeorm/entities/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import { startOfHour } from 'date-fns';
 
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,10 +3,10 @@ import 'reflect-metadata';
 import express, { Response, Request, NextFunction } from 'express';
 import 'express-async-errors';
 import routes from './routes';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 
-import './database';
+import '@shared/infra/typeorm';
 
 const app = express();
 
