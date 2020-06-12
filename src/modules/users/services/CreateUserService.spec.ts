2490db1c3306dfa0f4f114776866c39db9ca2ca2
Fabio Brito <fabio.britto15@gmail.com>
2020-06-12 10:52:09 -0300

    add test for the user services

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,48 @@
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import CreateUserService from './CreateUserService';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: '1234566',
+    });
+
+    expect(user).toHaveProperty('id');
+    expect(user.name).toBe('John Doe');
+  });
+
+  it('should not be able to create a new user with a email that already is use', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: '1234566',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'John Doe',
+        email: 'john@example.com',
+        password: '1234566',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
